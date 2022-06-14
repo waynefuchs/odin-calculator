6f2a4800e796b73645b66cee96327451975d5985
waynefuchs <104172375+waynefuchs@users.noreply.github.com>
2022-06-14 16:29:57 -0400

    Begin to make the operator buttons work

--- calculator.js
+++ calculator.js
@@ -13,11 +13,25 @@ const calculatorOperate = (f, a, b) => f(a, b);
 const lcda = document.querySelector('#lcd-a');
 const lcdb = document.querySelector('#lcd-b');
 const setDisplay = (display, value) => display.textContent = value;
-const displayHasDecimal = () => lcdb.textContent.includes('.');
-const addToDisplay = (value) => lcdb.textContent = 
-    (lcdb.textContent === "0") 
-        ? value 
-        : lcdb.textContent + value;
+const getDisplay = (display, value) => display.textContent;
+const hasDecimalInDisplay = () => lcdb.textContent.includes('.');
+const negativeDisplay = () => lcdb.textContent = String(Number(lcdb.textContent) * -1);
+const percentDisplay = () => lcdb.textContent = String(Number(lcdb.textContent) * 0.01);
+const addToDisplay = (value) => {
+    if(lcdb.textContent.length + 1 > 16) return;
+    const conversion = Number(lcdb.textContent);
+    if(isNaN(conversion)) {
+        console.error(`Input display contained value that was not a number: ${lcdb.textContent}`);
+        lcdb.textContent = "";
+        return;
+    }
+    // I'm not supporting numbers > 64 bit
+    if(conversion > 9007199254740991) return;
+    lcdb.textContent = 
+        (lcdb.textContent === "0") 
+            ? value 
+            : lcdb.textContent + value;
+};
 
 // 5.0.1. Figure out where to store everything
 let operandA;
@@ -25,6 +39,12 @@ let operandB;
 let operator;
 let result;
 let memory;
+const setOperator = (op) => {
+    console.log("???");
+    operator = op;
+    setDisplay(lcda, getDisplay(lcdb));
+    setDisplay(lcdb, 0);
+};
 const setCalculatorValuesToZero = (includeMemory=false) => {
     operandA = 0;
     operandB = 0;
@@ -35,38 +55,47 @@ const setCalculatorValuesToZero = (includeMemory=false) => {
     setDisplay(lcdb, 0);
 }; setCalculatorValuesToZero();
 
+// 5.2. Make the operators work
+const operators = {
+    divide: 0,
+    multiply: 0,
+    minus: 0,
+    plus: 0,
+};
+
+
 // 5.0.2. Wire up the JS to the HTML
 const buttonPressed = (e) => {
-    //console.log(e.target);
     const buttonAction = e.target.id.includes('-') ? e.target.id.split('-')[1] : e.target.id;
+    console.log(buttonAction);
     switch(buttonAction) {
-        case "allclear":
-            setCalculatorValuesToZero();
-            break;
         case "negative":
-            break;
+            negativeDisplay();
+            return;
         case "percent":
-            break;
+            percentDisplay();
+            return;
         case "divide":
-            break;
         case "multiply":
-            break;
-        case "subtract":
-            break;
-        case "add":
+        case "minus":
+        case "plus":
+            console.log("???!!!");
+            setOperator(buttonAction);
             break;
         case "equal":
             break;
+        case "allclear":
+            setCalculatorValuesToZero();
+            return;
         case "decimal":
-            if(!displayHasDecimal()) addToDisplay('.');
-            break;
+            if(!hasDecimalInDisplay()) addToDisplay('.');
+            return;
         default:
             if(buttonAction.match(/^[0-9]$/)) addToDisplay(buttonAction);
-            break;
+            return;
     }
 }
 const wireButtons = () => {
     const buttons = document.querySelectorAll('.button');
-    console.table(buttons);
     buttons.forEach(button => button.addEventListener('click', buttonPressed));
 }; wireButtons();
